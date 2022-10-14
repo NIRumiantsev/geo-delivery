fc81bb4b9dbe22914e779cb2dc63ed752c2802fa
nirumiantsev <nirumiantsev@gmail.com>
2022-10-14 19:35:18 +0400

    Add api modules

--- geo-delivery-api/src/app.module.ts
+++ geo-delivery-api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { DeliveryModule } from './delivery/delivery.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, OrderModule, DeliveryModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
